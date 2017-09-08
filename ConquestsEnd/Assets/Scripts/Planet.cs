671644fb181105cab4d58205139889a1a39ea154
areeser <areeser@email.sc.edu>
2017-09-08 14:14:39 -0400

    Added Planet Script
    Also started working on Text.

--- ConquestsEnd/Assets/Scripts/Planet.cs
+++ ConquestsEnd/Assets/Scripts/Planet.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Planet : MonoBehaviour {
 
+    public string controllingPlayer;
+    public int armies;
+    public Planet[] connections; //Test variable until map is created
+
+    public Text planetName;
+    public Text armiesDisplay;
 	// Use this for initialization
 	void Start () {
-		
+        connections = new Planet[10];
+        armiesDisplay.text = armies.ToString();
 	}
 	
 	// Update is called once per frame
