6b9df0c042ce2c978f70a2a66c9377dbc5f19f2d
areeser <areeser@email.sc.edu>
2017-09-15 13:22:51 -0400

    Planet Prefabs
    With Text!

--- ConquestsEnd/Assets/Scripts/Planet.cs
+++ ConquestsEnd/Assets/Scripts/Planet.cs
@@ -7,14 +7,18 @@ public class Planet : MonoBehaviour {
 
     public string controllingPlayer;
     public int armies;
+    public string planetName;
     public Planet[] connections; //Test variable until map is created
 
-    public Text planetName;
+    public Text planetNameTxt;
     public Text armiesDisplay;
 	// Use this for initialization
 	void Start () {
         connections = new Planet[10];
-        armiesDisplay.text = armies.ToString();
+        armiesDisplay.text = "Armies " + armies.ToString();
+        armiesDisplay.transform.position = gameObject.transform.position + new Vector3(0, 1.5f, 0);
+        planetNameTxt.text = planetName;
+        planetNameTxt.transform.position = gameObject.transform.position - new Vector3(0, 1.5f, 0);
 	}
 	
 	// Update is called once per frame
